04e1d5f18075a2ff2dc2d3a4bbf953f201e2caba
RYAN LI <essentialmomo@icloud.com>
2020-01-05 22:59:27 +0800

    new com

--- server/index.js
+++ server/index.js
@@ -41,7 +41,14 @@ passport.use(strategy);
 
 const app = express();
 app.use(passport.initialize());
-
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 // 解析表单 application/x-www-from-urlencodeed
 app.use(
   bodyParser.urlencoded({
