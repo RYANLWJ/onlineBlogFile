aa0fec100df6ede339fef6b5bd2dfdd34da9b859
RYAN LI <essentialmomo@icloud.com>
2020-01-05 23:44:16 +0800

    new com

--- server/index2.js
+++ server/index2.js
@@ -46,7 +46,8 @@ app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", ' 3.2.1');
-  res.header("Content-Type", "application/json;charset=utf-8");
+  res.header("Access-Control-Allow-Headers","content-type");
+  res.header("Content-Type", "'application/json;charset=UTF-8'");
   next();
 });
 // 解析表单 application/x-www-from-urlencodeed
